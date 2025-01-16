ef24a4778db445d19372dca2525bffdf626d7948
cubbbit <cubbbit@gmail.com>
2025-01-16 12:25:05 -0300

    Update settings.py

--- proyecto_vehiculos_django/settings.py
+++ proyecto_vehiculos_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=qyvp=uli81qd_^5og!_st=^mv$*@%bv^5i*@jfe8#wrr#vy6p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'proyecto_vehiculos_django.wsgi.app'
+WSGI_APPLICATION = 'proyecto_vehiculos_django.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
